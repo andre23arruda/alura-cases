7557c6fe841c0027afc19710ca795b2b8d09a83e
harpia01 <andre23arruda@gmail.com>
2022-02-15 16:54:45 -0400

    Add serverside props explain

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,6 +1,11 @@
 import Link from "../src/components/Link"
 
+// export async function getServerSideProps(context) {
+//     console.log('Em modo DEV sempre roda a cada acesso')
+//     console.log('Em modo PROD sempre roda a cada acesso')
 export async function getStaticProps(context) {
+    console.log('Em modo DEV sempre roda a cada acesso')
+    console.log('Em modo PROD só roda no build, pois gera os estáticos')
     const API_DATA_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     const faq = await fetch(API_DATA_URL)
         .then(response => {
@@ -17,7 +22,7 @@ export async function getStaticProps(context) {
 }
 
 export default function FaqPage({ faq }) {
-    console.log(faq)
+    // console.log(faq)
     return (
         <div>
             <h1>Alura Cases - FAQ</h1>
